760e11f0f834afb40f7237689872c3ebeefe6341
the2roock <maksympetrynko68@gmail.com>
2024-08-06 18:55:39 +0300

    refactor parser module

--- parser/depth.py
+++ parser/depth.py
@@ -1,57 +1,80 @@
-import asyncio
+from typing import List
 from datetime import datetime
 import pytz
-import traceback
+import asyncio
 
 from binance.client import AsyncClient
+from .config import BinanceConfig as Config
 
 from sqlalchemy.future import select
-from sqlalchemy import text
-
 from database.Base import async_connection
 from database.models.token import Symbol, OrderBook
 
-from config import BinanceConfig as Config
 
 
 async def get_symbols() -> list[Symbol]:
+    """Fetch symbol records from the database.
+
+    Returns:
+        List[Symbol]: List of ORM Symbol objects.
+    """
     session, engine = async_connection()
-    async with session() as db:
-        result = await db.execute(select(Symbol).filter(Symbol.status == 1))
-        symbols = [element[0] for element in result.all()]
-    await engine.dispose()
+    try:
+        async with session() as db:
+            result = await db.execute(select(Symbol).filter(Symbol.status == 1))
+            symbols = [element[0] for element in result.all()]
+    finally:
+        await engine.dispose()
     return symbols
 
 
-async def save_depths(symbol: Symbol, order_book: list[list], time_open: float = 0.0) -> None:
+async def save_depths(symbol: Symbol, order_book: List[List]) -> None:
+    """Insert new depths into the database.
+
+    Args:
+        symbol (Symbol): ORM Symbol object.
+        order_book (List[List]): Raw depth data.
+    """
     session, engine = async_connection()
-    async with session() as db:
-        depth_for_db = OrderBook(
-                id_symbol=symbol.id,
-                ask=order_book["ask"],
-                bid=order_book["bid"],
-                time_open=time_open
-            )
-        db.add(depth_for_db)
-        await db.commit()
-    await engine.dispose()
+    try:
+        async with session() as db:
+            depth_for_db = OrderBook(
+                    id_symbol=symbol.id,
+                    ask=order_book["ask"],
+                    bid=order_book["bid"],
+                    time_open=datetime.now(pytz.utc).replace(second=0, microsecond=0, tzinfo=None)
+                )
+            db.add(depth_for_db)
+            await db.commit()
+    finally:
+        await engine.dispose()
 
 
 async def main():
+    """Parse new order book values from Binance API and update the database."""
+    # Init Binance API object.
     BinanceAPI = AsyncClient(Config.api_key, Config.api_secret)
     
-    time_open = datetime.now(pytz.utc).replace(second=0, microsecond=0, tzinfo=None)
-    symbols = await get_symbols()
-    
-    for symbol in symbols:
-        depth = await BinanceAPI.get_order_book(symbol=symbol.symbol, limit=1000)
-        order_book = {
-            "bid": " ".join([f"{float(element[0])}:{float(element[1])}" for element in depth["bids"][::-1]]),
-            "ask": " ".join([f"{float(element[0])}:{float(element[1])}" for element in depth["asks"]]),
-        }
-        await save_depths(symbol, order_book, time_open)
-    
-    await BinanceAPI.close_connection()
+    try:
+        # Fetch symbols and tickers from DB.
+        symbols = await get_symbols()
+        
+        # Process symbols.
+        for symbol in symbols:
+            # Get current order book from the API.
+            depth = await BinanceAPI.get_order_book(symbol=symbol.symbol, limit=1000)
+            # Change depth`s data format to text.
+            order_book = {
+                "bid": " ".join([f"{float(element[0])}:{float(element[1])}" for element in depth["bids"][::-1]]),
+                "ask": " ".join([f"{float(element[0])}:{float(element[1])}" for element in depth["asks"]]),
+            }
+            # Save new order book.
+            await save_depths(symbol, order_book)
+            
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        await BinanceAPI.close_connection()
     
 
 if __name__ == "__main__":
--- parser/historical/klines.py
+++ parser/historical/klines.py
@@ -1,3 +1,4 @@
+from typing import List, Dict
 import asyncio
 from datetime import datetime
 from time import time
@@ -13,16 +14,32 @@ from database.models.token import Symbol, Kline
 from parser.config import BinanceConfig as Config
 
 
-async def get_symbols() -> list[Symbol]:
+async def get_symbols() -> List[Symbol]:
+    """Fetch symbol records from the database.
+
+    Returns:
+        List[Symbol]: List of ORM Symbol objects.
+    """
     session, engine = async_connection()
-    async with session() as db:
-        result = await db.execute(select(Symbol).filter(Symbol.status == 2))
-        symbols = [element[0] for element in result.all()]
-    await engine.dispose()
+    try:
+        async with session() as db:
+            result = await db.execute(select(Symbol).filter(Symbol.status == 2))
+            symbols = [element[0] for element in result.all()]
+    finally:
+        await engine.dispose()
     return symbols
 
 
-async def get_max_time_open_for_symbols(symbols: list[Symbol], time_if_None: int = 0) -> dict[Symbol, int]:
+async def get_max_time_open_for_symbols(symbols: List[Symbol], time_if_None: int = 0) -> Dict[Symbol, int]:
+    """Fetch symbols and last kline time_opens according to the symbols.
+
+    Args:
+        symbols (List[Symbol]): List of ORM Symbol objects.
+        time_if_None (int, optional): Value sets if it has never parsed before. Defaults to 0.
+
+    Returns:
+        Dict[Symbol, int]: ORM Symbol object and according time_opens.
+    """
     symbol_ids = tuple(symbol.id for symbol in symbols)
     sql_query = f"""
         select 
@@ -32,57 +49,77 @@ async def get_max_time_open_for_symbols(symbols: list[Symbol], time_if_None: int
         group by id_symbol
     """
     session, engine = async_connection()
-    async with session() as db:
-        result = await db.execute(text(sql_query))
-        max_time_open = {
-            element[0]: element[1]
-            for element in result.all()
-        }
-    await engine.dispose()
-    symbol_max_time_open = {symbol: int(max_time_open[symbol.id].timestamp()*1000) if symbol.id in max_time_open else time_if_None for symbol in symbols}
+    try:
+        async with session() as db:
+            result = await db.execute(text(sql_query))
+            max_time_open = {
+                element[0]: element[1]
+                for element in result.all()
+            }
+    finally:
+        await engine.dispose()
+    symbol_max_time_open = {
+        symbol: int(max_time_open[symbol.id].timestamp()*1000) 
+                if symbol.id in max_time_open 
+                else time_if_None 
+        for symbol in symbols
+    }
     return symbol_max_time_open
 
 
-async def save_into_db(klines: list[Kline]):
+async def save_into_db(klines: List[Kline]):
+    """Insert historical klines into the database.
+
+    Args:
+        klines (List[Kline]): List of ORM Kline objects.
+    """
     session, engine = async_connection()
-    async with session() as db:
-        db.add_all(klines)
-        await db.commit()
-    await engine.dispose()
+    try:
+        async with session() as db:
+            db.add_all(klines)
+            await db.commit()
+    finally:
+        await engine.dispose()
     
 
 async def main():
+    """Parse historical klines from Binance API and update the database."""
+    # Init Binance API object.
     BinanceAPI = AsyncClient(Config.api_key, Config.api_secret)
-    
+    # Fetch symbols with status = 2.
     symbols = await get_symbols()
     
+    # Fetch last kline time_opens according to the symbols.
     min_timestamp = 0
     max_saved_time_opens = await get_max_time_open_for_symbols(symbols=symbols, time_if_None=min_timestamp)
     
+    # Process symbols
     for symbol, start_timestamp in max_saved_time_opens.items():
-        klines_row = list()
         start_timestamp += 60 * 1000    # increment 60 seconds
         
-        # test start_timestamp
+        # start_timestamp value validation.
         klines = await BinanceAPI.get_klines(symbol=symbol.symbol, interval="1m", startTime=start_timestamp, limit=100)
         try:
             if start_timestamp < klines[0][0]:
                 start_timestamp = klines[0][0]
-                print(f"start time is confused and set to {datetime.fromtimestamp(start_timestamp/1000)}")
+                print(f"start_time is confused and set to {datetime.fromtimestamp(start_timestamp/1000)}")
         except Exception as e:
             print(f"The symbol {symbol.symbol} is broken: {e}\n")
             continue
         
+        # Process historical parsing.
         print(f"Downloading {symbol.symbol} data starts from {datetime.fromtimestamp(start_timestamp / 1000)}")
         t0 = time()    
         while start_timestamp < datetime.now().timestamp() * 1000:
+            # Get historical candles in '1m' time frame from the API.
             end_timestamp = start_timestamp + 1000 * 60 * 1000
             klines = await BinanceAPI.get_klines(symbol=symbol.symbol, interval="1m", startTime=start_timestamp, endTime=end_timestamp, limit=1000)
             print('time:', datetime.fromtimestamp(start_timestamp/1000), '--', datetime.fromtimestamp(end_timestamp/1000), 'length of data:', len(klines), end='\r')
             
+            # Apply next step.
             start_timestamp = end_timestamp
-            klines_row.extend(klines)
             
+            # Create ORM kline objects.
             klines_db = [
                 Kline(
                     id_symbol=symbol.id,
@@ -97,6 +134,8 @@ async def main():
                 )
                 for kline in klines
             ]
+            
+            # Save historical klines.
             await save_into_db(klines=klines_db)
             
         print(f"Data is downloaded. Time spent - {round(time() - t0, 2)}s\n")
